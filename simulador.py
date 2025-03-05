4fa90daabdc524edcd71f9307a3aa03d764d70a7
DeividArriaza <lop24730@uvg.edu.gt>
2025-03-05 16:59:50 -0600

    Se han ajustado algunos funcionamientos del código, pues se cree faltaba que cumplieran con lo solicitado, se dejó el funcionamiento de los procesos uno por uno, con su tiempo que se tarda en finalizar, desde el momento en que llega a la cola de la RAM, hasta que salen de la CPU. Esto se dejó así para poder ver uno por uno que se cumpliera con lo requerido

--- simulador.py
+++ simulador.py
@@ -1,50 +1,47 @@
 import simpy
 import random
 import numpy as np
-RANDOM_SEED = 28
+RANDOM_SEED = 25
 CPU_INSTRUCCIONES = 3 #modificable
 NUM_PROCESOS = 25 #Modificable
 interval = 10 #Modificable
 
-#Donde se generan los procesos aleatoriamente y se siguen hasta que se terminen
+#Donde se generan los procesos aleatoriamente y se siguen hasta que llegue a NUM_PROCESOS
 def proceso(env, ram, cpu, tiempos):
-    #Hasta que el mismo número de procesos sea el de terminados, se acaba la simulación
     for i in range(NUM_PROCESOS):
-        e = ejecucion(env, ram, cpu, tiempos)
+        e = ejecucion(env,'proceso%02d' % i, ram, cpu, tiempos)
         env.process(e)
         t = random.expovariate(1/interval)
         yield env.timeout(t)
 
-def ejecucion(env, ram, cpu, tiempos):
-    global terminated
-    usar_ram = random.randint(1,10)
-    proceso_instrucciones = random.randint(1,10)    
+
+def ejecucion(env, nombre, ram, cpu, tiempos):
     llegada = env.now
-    #Si se obtiene memoria, se pasa al estado de ready, de lo contrario sigue esperando hasta que pueda reicbir memoria
-    with ram.get(usar_ram) as req:
-        yield req
-        while proceso_instrucciones > 0:
-            #Ejecutar un proceso en la CPU
-            with cpu.request() as req_cpu:
-                yield req_cpu
-                yield env.timeout(1)  
-                if(proceso_instrucciones>=CPU_INSTRUCCIONES):
-                    proceso_instrucciones -= CPU_INSTRUCCIONES
-                else:
-                    proceso_instrucciones -= proceso_instrucciones
-            
-            #Estado terminated
-            if proceso_instrucciones == 0:
-                terminated = "Terminad"
+    usar_ram = random.randint(1,10)
+    proceso_instrucciones = random.randint(1,10)
+    yield ram.get(usar_ram)
+    estado = ""
+    while estado != "terminated":
+        #Ejecutar un proceso en la CPU
+        with cpu.request() as req_cpu:
+            yield req_cpu
+            yield env.timeout(1)  
+            if(proceso_instrucciones>=CPU_INSTRUCCIONES):
+                proceso_instrucciones -= CPU_INSTRUCCIONES
             else:
-                #Estado waiting
-                if random.randint(1, 2) == 1:
-                    yield env.timeout(1)
-                #Estado ready
-                else:
-                    continue
-        yield ram.put(usar_ram)
-        tiempos.append(env.now - llegada)
+                proceso_instrucciones -= proceso_instrucciones
+        if proceso_instrucciones == 0:
+            estado = "terminated"
+            print(nombre, "se tardó:", (env.now-llegada))
+            tiempos.append(env.now - llegada)
+            yield ram.put(usar_ram)
+        else:
+            if random.randint(1,2) == 1:
+                estado = "waiting"
+                yield env.timeout(1)
+            else:
+                estado = "ready"
+        
 
 #Inicialización de la simulación
 env = simpy.Environment()
@@ -56,6 +53,7 @@ random.seed(RANDOM_SEED)
 
 env.process(proceso(env, RAM, CPU, tiempos))
 env.run()
+
 print("Tiempo promedio para " + str(NUM_PROCESOS) + " procesos con " + str(interval) + " intervalos: " + str(np.mean(tiempos)))
 print("Desviación estándar para " + str(NUM_PROCESOS) + " procesos con " + str(interval) + " intervalos: " + str(np.std(tiempos)))
 
